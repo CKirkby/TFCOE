10c1a283a397b56ab51fd22f544f8b552bd58649
CKirkby <conorkirkby1121@gmail.com>
2025-11-01 16:40:07 +0000

    Initial Commit

---
+++ Source/TFCOE/TFCOE.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TFCOE.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TFCOE, "TFCOE" );
