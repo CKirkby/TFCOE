cf72c6d032a6ae0429ac85797f2416d0fe4dcb66
CKirkby <conorkirkby1121@gmail.com>
2025-11-19 00:40:40 +0000

    Prototyping Board piece tracing

--- Source/TFCOE/BoardPiece.cpp
+++ Source/TFCOE/BoardPiece.cpp
@@ -1,6 +1,8 @@
 // Created by Snow Paw Games
 
 #include "BoardPiece.h"
+#include "Engine/World.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 ABoardPiece::ABoardPiece()
 {
@@ -10,4 +12,61 @@ ABoardPiece::ABoardPiece()
 void ABoardPiece::BeginPlay()
 {
 	Super::BeginPlay();
+}
+
+TArray<FHitResult> ABoardPiece::ActivateCombatTrace(int Directions)
+{
+	TArray<FVector> DirectionsToTrace;
+
+	// Cardinal Directions
+	const FVector UpVector = FVector(0, 1, 0);
+	const FVector DownVector = FVector(0, -1, 0);
+	const FVector RightVector = FVector(1, 0, 0);
+	const FVector LeftVector = FVector(-1, 0, 0);
+
+	// Ordinal Directions
+	const FVector UpperLeftVector = FVector(-1, 1, 0).GetSafeNormal();
+	const FVector UpperRightVector = FVector(1, 1, 0).GetSafeNormal();
+	const FVector LowerLeftVector = FVector(-1, -1, 0).GetSafeNormal();
+	const FVector LowerRightVector = FVector(1, -1, 0).GetSafeNormal();
+
+	// Creates a mini binding function that if called, adds the direction linked to the directions to be checked. 
+	const ETraceDirections TraceFlags = static_cast<ETraceDirections>(Directions);
+	const TArray<FDirectionBinding> DirectionBindings =
+		{
+		{ETraceDirections::Up, [&]() {DirectionsToTrace.Add(UpVector);}}, //Adds the up direction
+		{ETraceDirections::Down, [&]() {DirectionsToTrace.Add(DownVector);}}, // Adds the down direction
+		{ETraceDirections::Right, [&]() {DirectionsToTrace.Add(RightVector);}}, // Adds the right vector
+		{ETraceDirections::Left, [&]() {DirectionsToTrace.Add(LeftVector);}}, // Adds the left vector
+		{ETraceDirections::UpperLeft, [&]() {DirectionsToTrace.Add(UpperLeftVector);}}, // Adds the upper left vector
+		{ETraceDirections::UpperRight, [&]() {DirectionsToTrace.Add(UpperRightVector);}}, // Adds the upper right vector
+		{ETraceDirections::LowerLeft, [&]() {DirectionsToTrace.Add(LowerLeftVector);}}, // Adds the lower left vector
+		{ETraceDirections::LowerRight, [&]() {DirectionsToTrace.Add(LowerRightVector);}} // Adds the lower right vector
+	};
+
+	// Checks which flag directions were set and triggers the action to bind it to the array of directions to check.
+	for (const FDirectionBinding& Binding : DirectionBindings)
+	{
+		if (EnumHasAllFlags(TraceFlags, Binding.Direction))
+		{
+			Binding.BindAction();
+		}
+	}
+
+	TArray<FHitResult> HitResults;
+
+	// Does a trace for each direction in the array to be checked
+	for (FVector Direction : DirectionsToTrace)
+	{
+		FVector TraceStart = GetActorLocation();
+		FVector TraceEnd = TraceStart + Direction * 350.0f;
+
+		TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes;
+		ObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECC_Pawn));
+		
+		UKismetSystemLibrary::LineTraceMultiForObjects(GetWorld(), TraceStart, TraceEnd, ObjectTypes,
+			false, TArray<AActor*>(), EDrawDebugTrace::ForDuration, HitResults, true);
+	}
+	
+	return HitResults;
 }
--- Source/TFCOE/BoardPiece.h
+++ Source/TFCOE/BoardPiece.h
@@ -16,6 +16,27 @@ enum EPieceState
 	Occupied UMETA(DisplayName = "Occupied"),
 };
 
+UENUM(BlueprintType, meta = (Bitflags, BitmaskEnum="ETraceDirections", UseEnumValuesAsMaskValuesInEditor="true"))
+enum class ETraceDirections : uint8
+{
+	None = 0 UMETA(Hidden),
+	Up = 1 << 0,
+	Down = 1 << 1,
+	Left = 1 << 2,
+	Right = 1 << 3,
+	UpperLeft = 1 << 4,
+	UpperRight = 1 << 5,
+	LowerLeft = 1 << 6,
+	LowerRight = 1 << 7
+};
+ENUM_CLASS_FLAGS(ETraceDirections)
+
+struct FDirectionBinding
+{
+	ETraceDirections Direction;
+	TFunction<void()> BindAction;
+};
+
 UCLASS()
 class TFCOE_API ABoardPiece : public AActor
 {
@@ -28,10 +49,16 @@ protected:
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Settings")
 	TEnumAsByte<EPieceState> CurrentPieceState = Enabled;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Settings")
+	AActor* CurrentOccupier = nullptr;
 	
 	virtual void BeginPlay() override;
 
 public:
+
+	UFUNCTION(BlueprintCallable, Category = "Board Piece")
+	TArray<FHitResult> ActivateCombatTrace(UPARAM(meta = (Bitmask, BitmaskEnum = "ETraceDirections")) int Directions);
 
 
 	// Getter and Setter
@@ -41,5 +68,28 @@ public:
 	{
 		CurrentPieceState = NewPieceState;
 	}
+
+	UFUNCTION(BlueprintCallable, Category = "Board Piece")
+	EPieceState GetPieceState() const
+	{
+		return CurrentPieceState;
+	}
+
+	UFUNCTION(BlueprintCallable, Category = "Board Piece")
+	AActor* GetCurrentOccupier() const
+	{
+		return CurrentOccupier;
+	}
+
+	UFUNCTION(BlueprintCallable, Category = "Board Piece")
+	void SetCurrentOccupier(AActor* NewOccupier)
+	{
+		if (NewOccupier)
+		{
+			CurrentOccupier = NewOccupier;
+		}
+	}
+
+	
 
 };
